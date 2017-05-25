961032efc719641fd04bef6593dbd4d011d6fa88
Gauthier Rodaro <gauthier@greatcircle.com>
2017-05-25 19:26:03 +0200

    [ADD] repo start from tuto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const freecom = {
+  companyName: 'Graphcool',
+  companyLogoURL: 'http://imgur.com/qPjLkW0.png',
+  mainColor: 'rgba(39,175,96,1)'
+}
+
+ReactDOM.render(<App freecom={freecom} />, document.getElementById('root'));
 registerServiceWorker();
