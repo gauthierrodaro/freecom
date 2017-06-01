e376439674a29f8c4fdd2c0dd450c247e9c821a5
Gauthier Rodaro <gauthier@greatcircle.com>
2017-06-01 11:57:02 +0200

    [ADD] appolo providr

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import logo from './logo.svg'
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient, {
+  createNetworkInterface,
+  ApolloProvider
+} from "react-apollo";
+
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import logo from "./logo.svg";
 
 const freecom = {
-  companyName: 'Rana',
+  companyName: "Rana",
   companyLogoURL: logo,
-  mainColor: 'rgba(0,105,255,1)'
-}
+  mainColor: "rgba(0,105,255,1)"
+};
 
-ReactDOM.render(<App freecom={freecom} />, document.getElementById('root'));
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: "https://api.graph.cool/simple/v1/cj3e8hr8g0wp50189apqq6ez1"
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App freecom={freecom} />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
