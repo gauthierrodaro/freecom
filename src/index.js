bdb308b224bf1987f365cacc9aeb4682407c6fbb
Gauthier Rodaro <gauthier@greatcircle.com>
2017-06-01 11:37:46 +0200

    [REF] set color and logo

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import logo from './logo.svg'
 
 const freecom = {
-  companyName: 'Graphcool',
-  companyLogoURL: 'http://imgur.com/qPjLkW0.png',
-  mainColor: 'rgba(39,175,96,1)'
+  companyName: 'Rana',
+  companyLogoURL: logo,
+  mainColor: 'rgba(0,105,255,1)'
 }
 
 ReactDOM.render(<App freecom={freecom} />, document.getElementById('root'));
